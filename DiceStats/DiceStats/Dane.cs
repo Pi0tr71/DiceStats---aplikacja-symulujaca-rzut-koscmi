ab72552b894421136c67e8deb89f22fb718a2145
Piotr Martenka <p-martenka71@outlook.com>
2022-08-08 15:00:52 +0200

    BarChart Colors

---
+++ DiceStats/DiceStats/Dane.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiceStats
+{
+    public static class Dane
+    {
+        public static int[] oczka = new int[11];
+        public static string[] kolory = new string[11];
+    }
+}
--- DiceStats/DiceStats/MainPage.xaml.cs
+++ DiceStats/DiceStats/MainPage.xaml.cs
@@ -7,43 +7,62 @@ using System.Threading.Tasks;
 using Microcharts;
 using SkiaSharp;
 using Xamarin.Forms;
+using Xamarin.Essentials;
 
 namespace DiceStats
 {
     public partial class MainPage : ContentPage
     {
-        public static class Oczka
-        {
-            public static int[] oczka = new int[11];
-        };
-        void DrawChart()
-        {
-            List<ChartEntry> DataList = new List<ChartEntry>();
-            for (int i = 0; i <= 10; i++)
-                DataList.Add(new ChartEntry(Oczka.oczka[i])
-                {
-                    Label = (i+2).ToString(),
-                    ValueLabel = Oczka.oczka[i].ToString(),
-                    Color = SKColor.Parse("#2c3e50")
-                });
-            chartViewBar.Chart = new BarChart { Entries = DataList, ValueLabelOrientation = Orientation.Horizontal, LabelOrientation = Orientation.Horizontal, LabelTextSize = 40 };
-            chartViewPie.Chart = new PieChart { Entries = DataList, HoleRadius = 0.3f, LabelTextSize = 30 };
-        }
         public MainPage()
         {
             InitializeComponent();
             var OczkaDefault = new int[11] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-            Oczka.oczka = OczkaDefault;
-            DrawChart();
-        }
+            var Kolory = new string[11] { "d44de1", "#da2ea2", "df0e62", "e63d4d", "fac70b", "c0b329", "869f46", "4c8b64", "127681", "1a4766", "21174a" };
+            Dane.oczka = OczkaDefault;
+            Dane.kolory = Kolory;
 
+        }
+        void DrawChart()
+        {
+            var suma = 0;
+            for (int i = 0; i <= 10; i++)
+            {
+                suma += Dane.oczka[i];
+            }
+            List<ChartEntry> DataList = new List<ChartEntry>();
+            List<ChartEntry> DataList2 = new List<ChartEntry>();
+            for (int i = 0; i <= 10; i++)
+            {
+                var procent = 0f;
+                if (Dane.oczka[i] != 0)
+                {
+                    procent = (float)Dane.oczka[i] / (float)suma * 100;
+                }
+                DataList.Add(new ChartEntry(Dane.oczka[i])
+                {
+                    Label = (i + 2).ToString(),
+                    ValueLabel = Dane.oczka[i].ToString(),
+                    Color = SKColor.Parse(Dane.kolory[i])
+                });
+                DataList2.Add(new ChartEntry(Dane.oczka[i])
+                {
+                    Label = (i + 2) + " - " + Math.Round(procent, 1) + "%",
+                    ValueLabel = Dane.oczka[i].ToString(),
+                    Color = SKColor.Parse(Dane.kolory[i])
+                });
+
+            }
+
+            chartViewBar.Chart = new BarChart { Entries = DataList, Margin = 40, BarAreaAlpha = 80, ValueLabelOrientation = Orientation.Horizontal, LabelOrientation = Orientation.Horizontal, LabelTextSize = 60 };
+            chartViewPie.Chart = new PieChart { Entries = DataList2, HoleRadius = 0.25f, LabelTextSize = 45 };
+        }
         private void Button_Clicked(object sender, EventArgs e)
         {
             var rand = new Random();
             int dice1 = rand.Next(1, 7);
             int dice2 = rand.Next(1, 7);
             Dice.Text = (dice1 + dice2).ToString();
-            Oczka.oczka[(dice1+dice2-2)] = Oczka.oczka[(dice1 + dice2 - 2)] + 1;
+            Dane.oczka[(dice1 + dice2 - 2)] = Dane.oczka[(dice1 + dice2 - 2)] + 1;
             DrawChart();
         }
     }
